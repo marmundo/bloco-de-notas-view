1d56f4ad6285b9e07c4d295298abcfb26c197fa3
Marcelo Damasceno <marcelodamasceno@gmail.com>
2022-12-03 18:38:21 -0300

    adiciona funcao de remover nota

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
-import {feedNotasContainer} from './script.js'
-document.onload=feedNotasContainer()
+import { getNota, getNotas,serverURL,authToken } from "./script.js"
+let notasContainer = document.querySelector('.corpo')
+let btnRemoverNota
+
+
+async function viewNota(id) {
+  let nota = getNota(id)
+  notasContainer.innerHTML = gerarNotaView(nota.titulo, nota.corpo)
+}
+
+async function deleteNota(e) {
+  let id=e.currentTarget.id
+  let url = `${serverURL}/${id}`
+  const response = await fetch(url, {
+    headers: { Authorization: `${authToken}`, 'Content-Type': 'application/json' },
+    method: 'DELETE',
+  })
+  if (!response.ok) {
+    console.log(response.json)
+  } else {
+    console.log("Nota Removida")
+    window.location.assign(".")
+  }
+}
+
+
+async function feedNotasContainer() {
+  let notas = await getNotas()
+  notas.forEach(nota => {
+    let cardHTML = gerarCard(nota)
+    notasContainer.innerHTML += cardHTML;
+  });
+  btnRemoverNota = document.querySelectorAll(".btnRemoverNota")
+  btnRemoverNota.forEach(btn => btn.addEventListener("click", deleteNota))
+
+}
+
+function gerarCard(nota) {
+  return `<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${nota.titulo}</h5>
+  <p class="card-text">${nota.corpo}.</p>
+  <!--<a href=javascript:viewNota(${nota.id}) class="btn btn-primary">Ver</a>-->
+  <a href=editarNota.html?id=${nota.id} class="btn btn-primary">Editar</a>
+  <button class="btn btn-primary btnRemoverNota" id=${nota.id} >Remover</button>
+</div>
+</div>`;
+}
+
+document.onload = feedNotasContainer()
+
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let notasContainer = document.querySelector('.corpo')
 export let serverURL = 'http://127.0.0.1:3333/api/notas'
 export let authToken = "Bearer NA.NE2DjYa4-jI_kJSI_kwP-K9k_L2p-VBYQzIasosCGvR1ZeuLlfff2atZp-yb"
 
@@ -27,36 +26,14 @@ async function getNotas() {
   return myJson
 }
 
-async function feedNotasContainer() {
-  let notas = await getNotas()
-  notas.forEach(nota => {
-    let cardHTML = gerarCard(nota)
-    notasContainer.innerHTML += cardHTML;
-  });
-}
-
-function gerarCard(nota) {
-  return `<div class="card" style="width: 18rem;">
-<div class="card-body">
-  <h5 class="card-title">${nota.titulo}</h5>
-  <p class="card-text">${nota.corpo}.</p>
-  <a href=javascript:viewNota(${nota.id}) class="btn btn-primary">Ver</a>
-  <a href=editarNota.html?id=${nota.id} class="btn btn-primary">Editar</a>
-  <a href=javascript:viewNota(${nota.id}) class="btn btn-primary">Remover</a>
-</div>
-</div>`;
-}
 
 function gerarNotaView(titulo, corpo) {
   return `<h1>${titulo}</h1><h4>${corpo}</h4>`
 }
 
-async function viewNota(id) {
-  let nota = getNota(id)
-  notasContainer.innerHTML = gerarNotaView(nota.titulo, nota.corpo)
-}
 
 
 
-export { getNota, feedNotasContainer, getFormJSONData }
+
+export { getNota, getFormJSONData, getNotas }
 
